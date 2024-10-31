6ef59b44d381b39d8860885025d30d93b8011b76
ccone87 <ccone833@boyslatin.net>
2024-10-31 14:20:42 +0000

    startingworkonpython

---
+++ Unit3_python/pythonfoundation.py
@@ -0,0 +1,32 @@
+# data types = datas are the fundamental building blocks
+# of python
+
+# string= character wrapped around quotation marks.
+" this is a sting//223093"
+"the computer does not care for spelling."
+"it only cares if the character we write are in quotes"
+
+#integers= whole numbers
+120
+11
+134
+
+# float = decimal numbers
+234.67
+986.9879
+
+# boolean = true or false value.
+true,1
+false,0
+
+# variable = a container for data
+# variables consist of three parts:
+# 1. we have a variable/ container name 
+# 2. we have the equal sign
+# 3. we have the value/ data
+
+johnB= 80
+mary= 80
+johnC= 80
+beth= 80
+tom= 80
